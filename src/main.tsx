6a6d2d526574ce01ef95482402a0d7e7d22add86
Aden Herold <aden.herold1@gmail.com>
2023-04-28 09:47:37 +1000

    build(deps): added normalize.css dependency

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@app/components/App";
 import "@fontsource/red-hat-text";
+import "normalize.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
