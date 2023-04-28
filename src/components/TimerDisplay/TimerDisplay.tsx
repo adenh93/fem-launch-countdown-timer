8d06afb934cd4874f22c9508b966193d2ab4f3e9
Aden Herold <aden.herold1@gmail.com>
2023-04-28 21:15:21 +1000

    feat: updated TimerDisplay component to accept value property, and pad value with leading zero

--- src/components/TimerDisplay/TimerDisplay.tsx
+++ src/components/TimerDisplay/TimerDisplay.tsx
@@ -1,19 +1,20 @@
-import { PropsWithChildren } from "react";
 import styles from "./TimerDisplay.module.css";
 
-export interface TimerDisplayProps extends PropsWithChildren {
+export interface TimerDisplayProps {
   label: string;
+  value: number;
 }
 
-export default function TimerDisplay({ label, children }: TimerDisplayProps) {
+export default function TimerDisplay({ label, value }: TimerDisplayProps) {
+  const formattedValue = String(value).padStart(2, "0");
+
   return (
-    <div className={styles.container}>
-      <div className={styles.background}>
-        <div className={styles.display}>
-          <span className={styles.leftDecoration} />
-          {children}
-          <span className={styles.rightDecoration} />
-        </div>
+    <div className={styles.flexContainer}>
+      <div className={styles.display}>
+        <span className={styles.fold} />
+        <span className={styles.leftDecoration} />
+        {formattedValue}
+        <span className={styles.rightDecoration} />
       </div>
       {label}
     </div>
