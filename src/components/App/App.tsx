2603acf6c0458b7e1b7cfd940d0a1534a5f3f950
Aden Herold <aden.herold1@gmail.com>
2023-04-28 15:11:31 +1000

    feat: added layout + styling to App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,23 @@
+import SocialIcons from "@app/components/SocialIcons";
+import patternHills from "@app/assets/images/pattern-hills.svg";
+import styles from "./App.module.css";
+
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <main className={styles.main}>
+      <div className={styles.content}>
+        <h1>We're launching soon</h1>
+        <h1>Timer</h1>
+      </div>
+      <img
+        className={styles.parallaxImg}
+        src={patternHills}
+        alt="Decorative illustration"
+      />
+      <footer className={styles.footer}>
+        <SocialIcons />
+      </footer>
+    </main>
   );
 }
 
