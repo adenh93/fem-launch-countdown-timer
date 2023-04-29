283b4109324ef9fb9cb85df3163e072cacd0149c
Aden Herold <aden.herold1@gmail.com>
2023-04-29 18:27:45 +1000

    build: added base to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/fem-launch-countdown-timer/",
   resolve: {
     alias: {
       "@app": path.resolve(__dirname, "./src"),
